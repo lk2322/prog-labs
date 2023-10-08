30a01336923d68e871a49dab15e0e6cd2190f66d
lk2322 <lk2322@ya.ru>
2023-10-08 13:37:38 +0300

    Add solutions to sudoku.py and test

--- src/lab3/sudoku.py
+++ src/lab3/sudoku.py
@@ -1,4 +1,5 @@
 import pathlib
+import random
 import typing as tp
 
 T = tp.TypeVar("T")
@@ -41,7 +42,7 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    pass
+    return [values[i:i + n] for i in range(0, len(values), n)]
 
 
 def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
@@ -53,7 +54,7 @@ def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str
     >>> get_row([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (2, 0))
     ['.', '8', '9']
     """
-    pass
+    return grid[pos[0]]
 
 
 def get_col(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
@@ -65,7 +66,7 @@ def get_col(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str
     >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
     ['3', '6', '9']
     """
-    pass
+    return [row[pos[1]] for row in grid]
 
 
 def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
@@ -78,7 +79,9 @@ def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[s
     >>> get_block(grid, (8, 8))
     ['2', '8', '.', '.', '.', '5', '.', '7', '9']
     """
-    pass
+    f = pos[0] // 3
+    s = pos[1] // 3
+    return [grid[f * 3 + i][s * 3 + j] for i in range(3) for j in range(3)]
 
 
 def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[int, int]]:
@@ -90,8 +93,10 @@ def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[in
     >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
-    pass
-
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == '.':
+                return i, j
 
 def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.Set[str]:
     """Вернуть множество возможных значения для указанной позиции
@@ -103,7 +108,7 @@ def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -
     >>> values == {'2', '5', '9'}
     True
     """
-    pass
+    return set('123456789') - set(get_row(grid, pos)) - set(get_col(grid, pos)) - set(get_block(grid, pos))
 
 
 def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
@@ -118,14 +123,26 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
+    f = find_empty_positions(grid)
+    if f is None:
+        return grid
+    else:
+        row, col = f
+    for i in find_possible_values(grid, f):
+        grid[row][col] = i
+        if solve(grid):
+            return grid
+        grid[row][col] = '.'
 
 
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
     """ Если решение solution верно, то вернуть True, в противном случае False """
     # TODO: Add doctests with bad puzzles
-    pass
-
+    for i in range(9):
+        for j in range(9):
+            if get_row(solution, (i, j)).count(solution[i][j]) > 1 or get_col(solution, (i, j)).count(solution[i][j]) > 1 or get_block(solution, (i, j)).count(solution[i][j]) > 1:
+                return False
+    return True
 
 def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
     """Генерация судоку заполненного на N элементов
@@ -148,7 +165,16 @@ def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
     >>> check_solution(solution)
     True
     """
-    pass
+    grid = [['.' for _ in range(9)] for _ in range(9)]
+    solve(grid)
+    N = 81 - N
+    while N != 0:
+        i = random.randint(0, 8)
+        j = random.randint(0, 8)
+        if grid[i][j] != '.':
+            grid[i][j] = '.'
+            N -= 1
+    return grid
 
 
 if __name__ == "__main__":
---
+++ tests/lab3/test_sudoku.py
@@ -0,0 +1,80 @@
+import unittest
+
+from src.lab3 import sudoku
+
+
+class SudokuTestCase(unittest.TestCase):
+    def test_get_row(self):
+        grid = sudoku.read_sudoku('puzzle1.txt')
+        self.assertEqual(sudoku.get_row(grid, (0, 0)), ['5', '3', '.', '.', '7', '.', '.', '.', '.'])
+        self.assertEqual(sudoku.get_row(grid, (4, 0)), ['4', '.', '.', '8', '.', '3', '.', '.', '1'])
+        self.assertEqual(sudoku.get_row(grid, (8, 0)), ['.', '.', '.', '.', '8', '.', '.', '7', '9'])
+
+    def test_get_col(self):
+        grid = sudoku.read_sudoku('puzzle1.txt')
+        self.assertEqual(sudoku.get_col(grid, (0, 0)), ['5', '6', '.', '8', '4', '7', '.', '.', '.'])
+        self.assertEqual(sudoku.get_col(grid, (0, 4)), ['7', '9', '.', '6', '.', '2', '.', '1', '8'])
+        self.assertEqual(sudoku.get_col(grid, (0, 8)), ['.', '.', '.', '3', '1', '6', '.', '5', '9'])
+
+    def test_get_block(self):
+        grid = sudoku.read_sudoku('puzzle1.txt')
+        self.assertEqual(sudoku.get_block(grid, (0, 0)), ['5', '3', '.', '6', '.', '.', '.', '9', '8'])
+        self.assertEqual(sudoku.get_block(grid, (4, 0)), ['8', '.', '.', '4', '.', '.', '7', '.', '.'])
+        self.assertEqual(sudoku.get_block(grid, (8, 0)), ['.', '6', '.', '.', '.', '.', '.', '.', '.'])
+
+    def test_find_empty_positions(self):
+        grid = sudoku.read_sudoku('puzzle1.txt')
+        self.assertEqual(sudoku.find_empty_positions(grid), (0, 2))
+        grid[0][2] = '1'
+        self.assertEqual(sudoku.find_empty_positions(grid), (0, 3))
+        grid[0][3] = '1'
+        self.assertEqual(sudoku.find_empty_positions(grid), (0, 5))
+
+    def test_find_possible_values(self):
+        grid = sudoku.read_sudoku('puzzle1.txt')
+        self.assertEqual(sudoku.find_possible_values(grid, (0, 2)), {'1', '2', '4'})
+        self.assertEqual(sudoku.find_possible_values(grid, (4, 0)), {'2', '9'})
+        self.assertEqual(sudoku.find_possible_values(grid, (8, 0)), {'1', '2', '3'})
+
+    def test_solve(self):
+        grid = sudoku.read_sudoku('puzzle1.txt')
+        sudoku.solve(grid)
+        self.assertEqual(grid, [['5', '3', '4', '6', '7', '8', '9', '1', '2'],
+                                ['6', '7', '2', '1', '9', '5', '3', '4', '8'],
+                                ['1', '9', '8', '3', '4', '2', '5', '6', '7'],
+                                ['8', '5', '9', '7', '6', '1', '4', '2', '3'],
+                                ['4', '2', '6', '8', '5', '3', '7', '9', '1'],
+                                ['7', '1', '3', '9', '2', '4', '8', '5', '6'],
+                                ['9', '6', '1', '5', '3', '7', '2', '8', '4'],
+                                ['2', '8', '7', '4', '1', '9', '6', '3', '5'],
+                                ['3', '4', '5', '2', '8', '6', '1', '7', '9']])
+
+        grid = sudoku.read_sudoku('puzzle2.txt')
+        sudoku.solve(grid)
+        self.assertEqual(grid, [['7', '9', '4', '5', '8', '2', '1', '3', '6'],
+                                ['2', '6', '8', '9', '3', '1', '7', '4', '5'],
+                                ['3', '1', '5', '4', '7', '6', '9', '8', '2'],
+                                ['6', '8', '9', '7', '1', '5', '3', '2', '4'],
+                                ['4', '3', '2', '8', '6', '9', '5', '7', '1'],
+                                ['1', '5', '7', '2', '4', '3', '8', '6', '9'],
+                                ['8', '2', '1', '6', '5', '7', '4', '9', '3'],
+                                ['9', '4', '3', '1', '2', '8', '6', '5', '7'],
+                                ['5', '7', '6', '3', '9', '4', '2', '1', '8']])
+    def test_check_solution(self):
+        grid = sudoku.read_sudoku('puzzle1.txt')
+        sudoku.solve(grid)
+        self.assertTrue(sudoku.check_solution(grid))
+        grid[0][0] = '3'
+        self.assertFalse(sudoku.check_solution(grid))
+        grid = sudoku.read_sudoku('puzzle2.txt')
+        sudoku.solve(grid)
+        self.assertTrue(sudoku.check_solution(grid))
+        grid[0][0] = '3'
+        self.assertFalse(sudoku.check_solution(grid))
+        grid = sudoku.read_sudoku('puzzle3.txt')
+        sudoku.solve(grid)
+        self.assertTrue(sudoku.check_solution(grid))
+        grid[0][0] = '3'
+        self.assertFalse(sudoku.check_solution(grid))
+
+
